e63587314860acbe26d114d7fecf15a552e7dc54
BioTheWolff <47079795+BioTheWolff@users.noreply.github.com>
2024-05-05 21:58:30 +0200

    feat: add team and memberships
    Signed-off-by: BioTheWolff <47079795+BioTheWolff@users.noreply.github.com>

--- github/locals.tf
+++ github/locals.tf
@@ -1,3 +1,4 @@
 locals {
-    members = csvdecode(file(var.members_filepath))
+    csv_file = csvdecode(file(var.members_filepath))
+    members = tomap({ for member in local.csv_file : member.name => member })
 }
--- github/main.tf
+++ github/main.tf
@@ -19,8 +19,24 @@ provider "github" {
 # Resources
 
 resource "github_membership" "members" {
-  for_each = tomap({ for member in local.members : member.name => member })
+  for_each = local.members
 
   username = each.key
   role = each.value.org_role
+}
+
+resource "github_team" "project_team" {
+  name = "${var.project} Team"
+}
+
+resource "github_team_members" "project_team_members" {
+  team_id  = github_team.team.id
+
+  dynamic "members" {
+    for_each = local.members
+    content {
+      username = members.key
+      role = members.value.team_role
+    }
+  }
 }
