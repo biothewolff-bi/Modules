9d23592f70b9b7f2680053e786ab51f0bb00f9c5
BioTheWolff <47079795+BioTheWolff@users.noreply.github.com>
2024-05-05 22:00:31 +0200

    feat: add project to repo, and its collaborators
    Signed-off-by: BioTheWolff <47079795+BioTheWolff@users.noreply.github.com>

--- github/main.tf
+++ github/main.tf
@@ -40,3 +40,21 @@ resource "github_team_members" "project_team_members" {
     }
   }
 }
+
+resource "github_repository" "project_repo" {
+  name = "${var.organization_name}.Project.${var.project}"
+}
+
+resource "github_repository_collaborators" "project_repo_collab" {
+  repository = github_repository.project_repo.name
+
+  user {
+    permission = "admin"
+    username   = var.project_admin
+  }
+
+  team {
+    permission = "push"
+    team_id    = github_team.project_team.slug
+  }
+}
--- github/variables.tf
+++ github/variables.tf
@@ -20,6 +20,11 @@ variable "organization_name" {
   type = string
 }
 
+variable "project_admin" {
+  description = "The username of the project administrator"
+  type = string
+}
+
 variable "members_filepath" {
   description = "File path to the CSV of github organisation and team members"
   type = string
